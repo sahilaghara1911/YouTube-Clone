0d34e44d91a7faea45550366845d03d8fa1808d6
[name] <nitinpatel9860@gmail.com>
2023-12-16 11:19:45 +0530

    DOne

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import SideBar from './SideBar'
 import MainContainer from './MainContainer'
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='p-5 w-[12rem]'>
+    <div className='flex flex-col'>
+    <ul className='space-y-2'>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Live</li>
+        <li>Video</li>
+      </ul>
+    </div>
+    <div>
+    <h1 className='font-bold'>Subscription</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+      <div>
+      <h1 className='font-bold py-5 pt-5'>Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      </div>
+      
+    </div>
   )
 }
 
