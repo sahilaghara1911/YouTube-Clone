d6cd92375931a6b3dc2878f5e5e27c8aa6e48afb
[name] <nitinpatel9860@gmail.com>
2023-12-17 19:27:58 +0530

    Done

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <div className='grid grid-flow-col p-2 m-2 '>
+    <div className='grid grid-flow-col p-2 m-2  '>
         <div className='flex col-span-1 gap-5'>
             <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEX///8AAACOjo7CwsItLS3c3Nw/Pz/6+vrGxsaSkpKZmZmIiIjy8vK+vr7w8PA4ODgfHx+3t7fi4uKY3kT7AAAAzklEQVR4nO3byQ3CQAxA0TGQDQhZ+i+WHCiAi2UU3uvga6Q52HJrAAAAAAAAAAAAAAAAAAAAAAA/4N7fKvX37MBHVHskF1b3HXIDn9V5h2du4lrdF2tuYOuqA6NLLmzDeK00DtmBAAAAAAB8dNOl0pQ+8p6rh/oxJxdW90X2du1VnXd45SZW50X2G7atui+25MLz/6UAAAAAAHyc/t5irx7qx55cuFQHxpIb+Ae3a9V5kb5dO/8N6fnvgAEAAAAAAAAAAAAAAAAAAAAAvvEGU4ASPASDdpgAAAAASUVORK5CYII=' className='h-8'/>
             <img src='https://ongpng.com/wp-content/uploads/2023/04/3-13.png' className='h-8 mx-2'/>
